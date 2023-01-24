1f7489d99dd48511986a067f0050a260e45d08e0
Tony Jinwoo Ahn <43518162+tony-jinwoo-ahn@users.noreply.github.com>
2023-01-24 14:13:54 +0900

    init

---
+++ main.js
@@ -0,0 +1,41 @@
+let canvas;
+let ctx;
+canvas = document.createElement("canvas");
+ctx = canvas.getContext("2d");
+canvas.width = 400;
+canvas.height = 700;
+document.body.appendChild(canvas);
+
+let backgroundImage, spaceshipImage, bulletImage, enemyImage, gameOverImage;
+let spaceshipX = canvas.width / 2 - 24;
+let spaceshipY = canvas.height - 48;
+
+function loadImage() {
+    backgroundImage = new Image();
+    backgroundImage.src = "images/background.jpg";
+
+    spaceshipImage = new Image();
+    spaceshipImage.src = "images/spaceship.png";
+
+    bulletImage = new Image();
+    bulletImage.src = "images/bullet.png";
+    
+    enemyImage = new Image();
+    enemyImage.src = "images/enemy.png";
+
+    gameOverImage = new Image();
+    gameOverImage.src = "images/gameover.png";
+}
+
+function render() {
+    ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(spaceshipImage, spaceshipX, spaceshipY);
+}
+
+function main() {
+    render();
+    requestAnimationFrame(main);
+}
+
+loadImage();
+main();
