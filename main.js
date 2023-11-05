1140b3f1b9bb92ee315e61d5f71393c6c7d029ec
Tony Jinwoo Ahn <tony.jinwoo.ahn@gmail.com>
2023-11-05 21:39:38 +0900

    Add bullet

--- main.js
+++ main.js
@@ -12,6 +12,20 @@ let spaceshipHeight = 48;
 let spaceshipX = (canvas.width / 2) - (spaceshipWidth/2);
 let spaceshipY = canvas.height - spaceshipHeight;
 
+let bulletList = []
+function Bullet() {
+    this.x = 0;
+    this.y = 0;
+    this.init = function() {
+        this.x = spaceshipX + 12;
+        this.y = spaceshipY;
+        bulletList.push(this);
+    }
+    this.update = function() {
+        this.y -= 10;
+    }
+}
+
 function loadImage() {
     backgroundImage = new Image();
     backgroundImage.src = "images/background.jpg";
@@ -36,8 +50,17 @@ function setupKeyboardListener() {
     });
     document.addEventListener("keyup",function(event){
         delete keysDown[event.key];
+        if (event.code == "Space") {
+            createBullet();
+        }
     })
 }
+
+function createBullet() {
+    let b = new Bullet();
+    b.init();
+}
+
 function update() {
     if ("ArrowRight" in keysDown) {
         if (spaceshipX < canvas.width-spaceshipWidth) {
@@ -49,11 +72,17 @@ function update() {
             spaceshipX -= 5;
         }
     }
+    for (let i=0 ; i<bulletList.length ; i++) {
+        bulletList[i].update();
+    }
 }
 
 function render() {
     ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
     ctx.drawImage(spaceshipImage, spaceshipX, spaceshipY);
+    for (let i=0 ; i<bulletList.length ; i++) {
+        ctx.drawImage(bulletImage, bulletList[i].x, bulletList[i].y);
+    }
 }
 
 function main() {
